8a9b79ddf74da02526ccd7cb08016a2921932391
wojole94 <wojciech.oleszko@gmail.com>
2023-08-02 10:21:33 +0200

    *Initial commit to repo

---
+++ src/main/java/com/xmcy/test/recommendation/service/RecomendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xmcy.test.recommendation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xmcy/test/recommendation/service/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmcy.test.recommendation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
